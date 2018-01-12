2144c64e1e1c6e054a9ec6fc3ba34f44d7dd88b6
Mateus Braga <mateuscbraga@gmail.com>
2018-01-12 21:20:13 +0000

    ageCalculator.js-exer2

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+	
+  return(name + " is " + (currentYear - yearOfBirth) + " years old.")
+}
+
+console.log(ageCalculator("Suzi", 1983, 2015));
+console.log(ageCalculator("Jack", 2003, 2015));
+console.log(ageCalculator("Ali", 2015, 2015));
